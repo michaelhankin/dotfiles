a564b1ce77105cf60e51c563ca1629ab017b6b2b
Michael Hankin <me@michaelhankin.com>
2020-07-22 11:08:06 -0500

    new alias

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,6 +4,7 @@ alias view='nvim -M'
 alias python='python3'
 alias pip='python -m pip'
 alias rmd='rm -rf'
+alias brew_up='brew upgrade && brew cask upgrade'
 
 # Erase fish greeting
 set -U fish_greeting ""
@@ -13,5 +14,9 @@ set -x LC_ALL en_US.UTF-8
 
 if command -v pyenv 1>/dev/null 2>&1
   pyenv init - | source 
+end
+
+if test -e ~/.config/bb_config.fish
+  source ~/.config/bb_config.fish
 end
 
